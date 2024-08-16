4cdd5ab255c293804dc400e6dda4916595c97ad8
JakKepka <kubakepka503@gmail.com>
2024-08-16 16:36:23 +0200

    Poprawa algorytmu semi-supervised-fcm, inicjalizacja macierzy semi-supervised labels

--- libraries/IFCM.py
+++ libraries/IFCM.py
@@ -104,11 +104,11 @@ def cmeans0(data, centroids, metric, c, m):
     return centroids, fuzzy_labels, jm, dist
 
 
-def incremental_fuzzy_cmeans(data, c, m, error, maxiter, metric = 'euclidean', init_centroid=None, m_low_boost = True, m_low = 1.1, m_low_iter = 3):
+def incremental_fuzzy_cmeans(data, c, m, error, maxiter, metric = 'euclidean', init_centroid=None, m_low_boost = False, m_low = 1.1, m_low_iter = 3):
     # data jeste postaci (n_samples, k_features)
     
     # Struktura do której bedziemy zbierać informacje z każdej iteracji
-    statistics = Multilist(['fpc', 'jm'])
+    statistics = Multilist(['fpc'])
     
     centroids = init_centroid
     
@@ -131,7 +131,7 @@ def incremental_fuzzy_cmeans(data, c, m, error, maxiter, metric = 'euclidean', i
             [centroids, fuzzy_labels, Jjm, dist] = cmeans0(data, centroids_copy, metric, c, m)
 
         fpc = _fp_coeff(fuzzy_labels)
-        statistics.add_elements([fpc, Jjm/data.shape[0]])
+        statistics.add_elements([fpc])
         p += 1
         
         # Stopping rule
